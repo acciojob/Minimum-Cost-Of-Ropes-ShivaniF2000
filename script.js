b53cf411c0b71b7fd7e4dde04d63a3d7b27e93da
ShivaniF2000 <119884719+ShivaniF2000@users.noreply.github.com>
2023-04-20 11:59:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function calculateMinCost() {
   //your code here
   
-  
+  const input = document.getElementById("rope-lengths").value;
+    const ropes = input.split(",").map(Number);
+    ropes.sort((a, b) => a - b);
+    let cost = 0;
+    while (ropes.length > 1) {
+      const sum = ropes[0] + ropes[1];
+      cost += sum;
+      ropes.splice(0, 2, sum);
+      ropes.sort((a, b) => a - b);
+    }
+    document.getElementById("result").innerHTML =
+      "Minimum cost of ropes: " + cost;
   
 }  
